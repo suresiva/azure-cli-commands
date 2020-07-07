090eaa9a14156ebdb433a9d3394d29b401c0fac6
suresiva <suresh1988.siva@gmail.com>
2020-07-07 15:53:58 -0400

    added cosmos key list

--- cosmosdb/general-operations-commands.sh
+++ cosmosdb/general-operations-commands.sh
@@ -16,3 +16,7 @@ az cosmosdb collection list --subscription demo-sub-1 --resource-group-name demo
 az cosmosdb collection show --subscription demo-sub-1 --resource-group-name demo-rg-1 --name demo-cosmos-account-name --db-name db-name-1 --collection-name test_coll-1
 #can also do it with sql api
 az cosmosdb sql container show --subscription demo-sub-1 --resource-group demo-rg-1 --account-name demo-cosmos-account-name --database-name db-name-1 --name test_coll-1
+
+
+# to show the keys (primary/secondary) of the database account
+az cosmosdb keys list --subscription demo-sub-1 --resource-group demo-rg-1 --name demo-cosmos-account-name
