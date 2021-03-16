9aea7b3c0abc60e1d8a1baae91a0a9fdf779bbed
suresiva <49594747+suresiva@users.noreply.github.com>
2021-03-16 16:16:22 -0400

    Update and rename function_init_cmds.sh to function_all_cmds.sh

--- azure-functions/function_all_cmds.sh
+++ azure-functions/function_all_cmds.sh
@@ -9,3 +9,7 @@ func init --worker-runtime python --language python
 
 # to create a function into the current function app directory
 func function -n trial-input-trigger -l python -t 'Durable Functions HTTP starter'
+
+
+# to start a azure function app
+func host start
