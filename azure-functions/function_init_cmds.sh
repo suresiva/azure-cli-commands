65213addc10c1af39b1bc4c5fc55c714623dbef8
suresiva <49594747+suresiva@users.noreply.github.com>
2021-03-16 16:15:12 -0400

    Update function_init_cmds.sh

--- azure-functions/function_init_cmds.sh
+++ azure-functions/function_init_cmds.sh
@@ -3,5 +3,9 @@
 # to list available templates for function apps for python language
 func templates list -l python
 
-# to create a python function app using a trigger
+# to initialize a python function app using a trigger
+# executed under a directory
+func init --worker-runtime python --language python
+
+# to create a function into the current function app directory
 func function -n trial-input-trigger -l python -t 'Durable Functions HTTP starter'
