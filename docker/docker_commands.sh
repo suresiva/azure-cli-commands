37e2f8d04061a7ac29d5ae5d59cf8c270ecce6ba
suresiva <49594747+suresiva@users.noreply.github.com>
2021-04-13 15:38:48 -0400

    Update docker_commands.sh

--- docker/docker_commands.sh
+++ docker/docker_commands.sh
@@ -13,3 +13,12 @@ docker ps
 #stop running container
 docker stop <container-id>
 
+#image buiding
+docker build --tag <image-name>:<latest/version> .
+
+#run docker image
+docker run <image-name>:<latest/version>
+#interactive
+docker run -it <image-name>:<latest/version>
+#with environment variables
+docker run -it -e <env-var-name>='<env-var-value>' <image-name>:<latest/version>
