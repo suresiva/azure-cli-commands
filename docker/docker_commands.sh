cbf9dbffeb665c539239fd108965d5362fcd8a77
suresiva <49594747+suresiva@users.noreply.github.com>
2021-04-13 12:05:40 -0400

    Create docker_commands.sh

---
+++ docker/docker_commands.sh
@@ -0,0 +1,15 @@
+#login into a docker account
+docker login <account-name>
+
+#login into a jfrog artifactory host
+docker login <account-name>.jfrog.io
+
+#list docker images
+docker images
+
+#list running contaiers
+docker ps
+
+#stop running container
+docker stop <container-id>
+
