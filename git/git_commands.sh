493d85bc9f9e6ca990b2598a46fa24f42a5b0abf
suresiva <49594747+suresiva@users.noreply.github.com>
2021-04-27 15:25:46 -0400

    Create git_commands.sh

---
+++ git/git_commands.sh
@@ -0,0 +1,9 @@
+# to list local git profile config
+git config list
+
+# to list only the user name or email
+git config user.name
+git config user.email
+
+# to modify any config
+git config --global user.name 'Suresh Siva'
